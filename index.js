bc3889cebd670eff1eb9c51261f0b8cf907b7b20
Adam DePaolo <amdepaolo@gmail.com>
2022-04-15 17:38:43 -0400

    commit tree 6e95ca573f8894b9187fe990fd99281d75038a46

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response => response.json())
+    .then(appendToDoc)
+    .catch(errorAppender)
+}
+
+function appendToDoc(object){
+    console.log(object.id)
+    document.body.innerHTML = `
+    <div>${object.id}</div>`
+}
+
+function errorAppender(err){
+    console.log(err);
+    document.body.innerHTML = `
+    <div>${err}</div>`
+}
